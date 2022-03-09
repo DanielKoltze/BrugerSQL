31b88648c6f3b29585fe9868d34d757b9848bff3
DanielKoltze <danielkoltze@yahoo.dk>
2022-03-09 10:55:14 +0100

    branch

--- src/repositores/UserRepository.java
+++ src/repositores/UserRepository.java
@@ -15,10 +15,12 @@ public class UserRepository {
     private static Connection connection;
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
         Menu menu = new Menu("Options til brugere i", "Vælg en af overstående", new String[]{"1. Opret", "2. Update", "3. Slet", "4. Hent", "5. luk program"});
         boolean isRunning = true;
         connectToMySQL();
+        int userId;
+        Scanner input = new Scanner(System.in);
         while (isRunning) {
             menu.printMenu();
             switch (menu.readChoice()) {
@@ -27,14 +29,18 @@ public class UserRepository {
                     String name = input.next();
                     System.out.println("Skriv kodeordet:");
                     String password = input.next();
-                    insertData(name, password);
+                    System.out.println("Skriv gruppe id: 1=guest  2=student");
+                    insertData(name, password, gruppeId);
                     System.out.println("Du har tilføjet " + name + " " + password);
                     break;
                 case 2:
+                    System.out.println("Skriv id på den bruger du gerne vil opdatere:");
+                    userId = input.nextInt();
+                    updateUser(userId);
                     break;
                 case 3:
                     System.out.println("Skriv ID på den bruger du vil slette");
-                    int userId = input.nextInt();
+                    userId = input.nextInt();
                     deleteUser(userId);
                     break;
                 case 4:
@@ -52,14 +58,16 @@ public class UserRepository {
     static void showAllRows() {
         try {
             Statement statement = connection.createStatement();
-            String sql = "SELECT * FROM brugere";
+            String sql = "SELECT * FROM brugere INNER JOIN gruppe ON gruppe.gruppe_id = brugere.gruppe_id";
             ResultSet resultSet = statement.executeQuery(sql);
 
             while (resultSet.next()) {
                 int userId = resultSet.getInt("id_bruger");
-                String name = resultSet.getString("brugernavn");
+                String brugernavn = resultSet.getString("brugernavn");
                 String password = resultSet.getString("kodeord");
-                System.out.println(userId + ". " + name + " " + password);
+                String name = resultSet.getString("name");
+                String rettigheder = resultSet.getString("rettigheder");
+                System.out.println(userId + ". " + brugernavn + " " + password + "         " + name + "  " + rettigheder);
 
             }
         } catch (Exception e) {
@@ -67,12 +75,13 @@ public class UserRepository {
         }
     }
 
-    static void insertData(String userName, String password) {
-        String query = "INSERT INTO brugere VALUES (null,?,?);";  //null er fordi den er autoincromented og ? er fordi vi sætter dem senere
+    static void insertData(String userName, String password, int gruppeId) {
+        String query = "INSERT INTO brugere VALUES (null,?,?,?);";  //null er fordi den er autoincromented og ? er fordi vi sætter dem senere
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setString(1, userName);
             preparedStatement.setString(2, password);
+            preparedStatement.setInt(3, gruppeId);
             preparedStatement.executeUpdate();
         } catch (Exception e) {
             System.out.println("Indsatte ikke række");
@@ -82,7 +91,6 @@ public class UserRepository {
     static void connectToMySQL() {
         try {
             connection = DriverManager.getConnection(DB_URL, user, password);
-            System.out.println("Virker nu");
         } catch (Exception e) {
             System.out.println("Virker ikke");
         }
@@ -125,6 +133,28 @@ public class UserRepository {
         }
         return selectedUser;
 
+
+    }
+
+    public static void updateUser(int userId){
+        Scanner input = new Scanner(System.in);
+        User selectedUser = selectUser(userId);
+        System.out.println("Du er igang med at opdatere " + selectedUser);
+        System.out.println("Indtast det nye brugernavn:");
+        String newBrugernavn = input.next();
+        System.out.println("Indtast det nye kodeord:");
+        String newKodeord = input.next();
+        String query = "UPDATE brugere SET brugernavn = '" + newBrugernavn + "', kodeord= '" + newKodeord + "' WHERE id_bruger = " + userId;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.executeUpdate();
+            System.out.println("You updated: " + selectedUser + " to " + userId + " " + newBrugernavn + " " + newKodeord);
+
+        } catch (Exception e) {
+            System.out.print("kunne ikke slette bruger: ");
+            e.printStackTrace();
+
+        }
 
     }
 
